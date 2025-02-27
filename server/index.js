77adca32927505db58463a1d39639abed988399a
Abdumuhammadjon <nestnext551@gmail.com>
2025-02-27 08:18:22 -0500

    ggg

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const pool = require("./postgres/db.js"); // db.js ni import qildik
+require("dotenv").config();
 
-require('dotenv').config()
-PORT  = process.env.PORT
-app.get("/" , (req, res) =>{
-    res.send('hello world')
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,   () =>{
-    console.log('baza ishladi')
-})
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()"); // Bazaga test so‘rov
+    res.send(`Baza ishladi: ${result.rows[0].now}`);
+    console.log('databaza ishladi')
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server xatosi");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} portda ishladi`);
+});
