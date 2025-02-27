744d02bcde014e272288d79cf1f6dd8ee8df64f7
Abdumuhammadjon <nestnext551@gmail.com>
2025-02-27 06:45:30 -0500

    fff

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 
 require('dotenv').config()
-
+PORT  = process.env.PORT
 app.get("/" , (req, res) =>{
     res.send('hello world')
 })
 
-app.listen(5000,   () =>{
+app.listen(PORT,   () =>{
     console.log('baza ishladi')
 })
