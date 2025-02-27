e5db0ea370a0782ba38aa57a3bd5cc08b5ca9e95
Abdumuhammadjon <nestnext551@gmail.com>
2025-02-27 07:43:49 -0500

    ggggg

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Mening Saytim</title>
+        <link rel="icon" href="https://cdn-icons-png.flaticon.com/128/4688/4688995.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+
+
+
+
+  )
 }
