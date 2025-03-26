fe93edcd1238acdd58727624bf2795e41d1c9834
Abdumuhammadjon <nestnext551@gmail.com>
2025-03-26 10:06:00 +0500

    adadasdasd

--- server/config/supabaseClient.js
+++ server/config/supabaseClient.js
@@ -1,7 +1,5 @@
 const { createClient } = require("@supabase/supabase-js");
-require("dotenv").config(); // .env fayldan Supabase ma'lumotlarini yuklash
 
-// Supabase client yaratish
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-module.exports = supabase; // Supabase ni eksport qilish
+module.exports = { supabase };
