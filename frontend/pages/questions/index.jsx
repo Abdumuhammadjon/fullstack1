e99d4c372512787eb4106a590ec2e07f346ac850
Abdumuhammadjon <nestnext551@gmail.com>
2025-03-28 08:54:14 +0500

    ddds

--- frontend/pages/questions/index.jsx
+++ frontend/pages/questions/index.jsx
@@ -92,7 +92,7 @@ export default function Admin() {
   };
 
   return (
-    <div className="flex flex-col h-screen bg-gray-100">
+    <div className="flex -ml-5  flex-col h-screen bg-gray-100">
       <Head>
         <title>Admin Paneli</title>
         <meta name="description" content="Savollar va variantlar qo‘shish" />
