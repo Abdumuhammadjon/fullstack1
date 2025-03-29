04cd2d39ace909e6de149ac42afc44f982322ff0
Abdumuhammadjon <nestnext551@gmail.com>
2025-03-29 23:46:20 +0500

    rttetret

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -26,10 +26,12 @@ App.getInitialProps = async ({ Component, ctx }) => {
   let user = null;
   try {
     const response = await Axios.get("http://localhost:5001/auth/profile", {
+      withCredentials: true,
       headers: {
         cookie: ctx?.req?.headers?.cookie,
       },
     });
+    
     user = response.data;
   } catch (error) {
     if (error.response && error.response.status === 401) {
