c70f077cfd79d91cb13f2fe416cb0b69c55b2075
Abdumuhammadjon <nestnext551@gmail.com>
2025-03-29 22:35:43 +0500

    dfdfd

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,34 +1,17 @@
 import "@/styles/globals.css";
 import Head from "next/head";
 import Layout from "../components/Layout";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import Axios from "axios";
 
 export default function App({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get("http://localhost:5001/auth/profile", {
-          withCredentials: true, // Cookie’ni yuborish
-        });
-        setUser(response.data);
-      } catch (error) {
-        console.error("Xatolik:", error.response?.data || error.message);
-        setUser(null);
-      }
-    };
-    fetchUser();
-  }, []);
-
+  const { user } = pageProps;
   return (
     <>
       <Head>
         <title>Mening Saytim</title>
         <link
           rel="icon"
-          href="https://cdn-icons-png.flaticon.com/128/4688/4688995.png"
+          href="https://cdnicons-png.flaticon.com/128/4688/4688995.png"
         />
       </Head>
       <Layout>
@@ -37,3 +20,34 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
+
+App.getInitialProps = async ({ Component, ctx }) => {
+  const { asPath } = ctx;
+  let user = null;
+  try {
+    const response = await Axios.get("http://localhost:5001/auth/profile", {
+      headers: {
+        cookie: ctx?.req?.headers?.cookie,
+      },
+    });
+    user = response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      if (asPath !== "/Login") {
+        // Redirect to login page
+        if (ctx.res) {
+          ctx.res.writeHead(302, { Location: "/Login" });
+          ctx.res.end();
+        }
+        return {};
+      } else {
+        // It's login page, allow without token
+        user = null;
+      }
+    } else {
+      // Some other error
+      throw error;
+    }
+  }
+  return { pageProps: { user } };
+};
