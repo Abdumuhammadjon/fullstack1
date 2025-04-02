77c88d5283971dd88646f611de8649e7e0466956
Abdumuhammadjon <nestnext551@gmail.com>
2025-04-02 11:59:04 +0500

    sdadsad

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 import Link from "next/link"; 
+import { LogOut } from 'lucide-react';
 
 const Navbar = () => {
+
+  const handleLogout = () => {
+    // Cookiesni o'chirish
+    document.cookie.split(";").forEach(function(cookie) {
+      const name = cookie.split("=")[0].trim();
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    });
+
+    // localStorage'ni tozalash
+    localStorage.clear();
+    
+    // sessionStorage'ni tozalash
+    sessionStorage.clear();
+
+    // Login sahifasiga yo'naltirish
+    router.push('/Login');
+  };
+
   return (
     <nav style={styles.navbar}>
     <h1 style={styles.logo}>MyApp</h1>
@@ -9,6 +28,9 @@ const Navbar = () => {
       <li><Link href="/">Home</Link></li>
       <li><Link href="/about">About</Link></li>
       <li><Link href="/contact">Contact</Link></li>
+      <li className="flex items-center gap-3 hover:bg-gray-700 p-2 rounded-lg cursor-pointer" onClick={handleLogout}>
+  <LogOut size={24} /> { "Chiqish"}
+</li>
       
     </ul>
   </nav>
